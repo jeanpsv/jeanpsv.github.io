0c38acbe5df23f5cd95115ea3869cab8c64bf40f
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2015-12-03 19:33:10 -0200

    close #4 - Aprimora exibição em telas menores.

--- app/components/maintenance/maintenance.js
+++ app/components/maintenance/maintenance.js
@@ -2,13 +2,5 @@
 
 angular.module('jeanpsv.github.io')
 .controller('MaintenanceController', ['$scope', function($scope) {
-	$scope.contacts = [{
-		name: 'github',
-		url: 'https://github.com/jeanpsv',
-		icon: 'assets/img/social/github-circle.svg'
-	}, {
-		name: 'linkedin',
-		url: 'https://br.linkedin.com/in/jean-vasconcelos-823247103',
-		icon: 'assets/img/social/linkedin.svg'
-	}]
+	
 }]);
